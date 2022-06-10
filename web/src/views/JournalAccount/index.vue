a6876ff2335f3c0ce1b29fffb1e7d5a7cd120c90
Qiao Anran <qiaoanran@gmail.com>
2022-06-10 16:51:49 +0800

    feat: add simple transactions data table

---
+++ web/src/views/JournalAccount/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="transactions"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: "JournalAccount",
+  data() {
+    return {
+      headers: [
+        {
+          text: "账单时间",
+          align: "start",
+          sortable: false,
+          value: "time",
+        },
+        { text: "账单类型", value: "type" },
+        { text: "账单分类", value: "category" },
+        { text: "账单金额", value: "amount" },
+      ],
+
+      transactions: [],
+    };
+  },
+};
+</script>
