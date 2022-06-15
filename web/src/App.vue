b61526c5cda43b6ca8cc2fb9cd3cd138404fbf9a
Qiao Anran <qiaoanran@gmail.com>
2022-06-15 19:07:44 +0800

    feat: add menus

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,55 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item-group :value="menu" active-class="deep-purple--text text--accent-4">
+        <v-list-item v-for="(m, index) in menus" :key="index" :to="m.path">
+          <v-list-item-title>{{ m.label }}</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-navigation-drawer>
+
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    drawer: false,
+    menu: null,
+    menus: [
+      {
+        path: "/journal-account",
+        label: "账单",
+      },
+      {
+        path: "/categories",
+        label: "账单分类",
+      },
+    ],
   }),
 };
 </script>
