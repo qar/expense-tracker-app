5fd6b6d162a0d0e8378a70d126a48004a9299e7b
Qiao Anran <qiaoanran@gmail.com>
2022-06-15 23:18:15 +0800

    fix: update menu and default router

--- web/src/App.vue
+++ web/src/App.vue
@@ -29,7 +29,7 @@ export default {
     menu: null,
     menus: [
       {
-        path: "/journal-account",
+        path: "/",
         label: "账单",
       },
       {
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +7,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import(/* webpackChunkName: "about" */ "../views/JournalAccount/index.vue"),
   },
   {
     path: "/about",
@@ -17,11 +16,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/journal-account",
-    name: "JournalAccount",
-    component: () => import(/* webpackChunkName: "about" */ "../views/JournalAccount/index.vue"),
   },
 ];
 
