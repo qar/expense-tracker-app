ef8b35290e329caec9b1c908c8467ecf61b45a5b
Qiao Anran <qiaoanran@gmail.com>
2022-06-21 19:32:35 +0800

    update pages

---
+++ precache-manifest.5acb2adb621346aee6cd2daa5289ed79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c944a63a4c696a215277",
+    "url": "/css/about.3a590fe8.css"
+  },
+  {
+    "revision": "0335accf2e2bd12abf50",
+    "url": "/css/chunk-vendors.7f342148.css"
+  },
+  {
+    "revision": "e935eb01c52ca37b86f6525fa95c7a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c944a63a4c696a215277",
+    "url": "/js/about.a00d6525.js"
+  },
+  {
+    "revision": "ae4351274ead1563d11c",
+    "url": "/js/app.05273ba8.js"
+  },
+  {
+    "revision": "0335accf2e2bd12abf50",
+    "url": "/js/chunk-vendors.4b5e9413.js"
+  },
+  {
+    "revision": "4ccad91926a6e3abd713585ec10360bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
