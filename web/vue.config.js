7a362db8ec59550d8ee591f824beae08aed5582b
Qiao Anran <qiaoanran@gmail.com>
2022-06-21 21:59:23 +0800

    feat: add a build script for github pages

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
 
+  publicPath: process.env.VUE_APP_FOR_GH_PAGES ? "." : "/",
+
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "My Expenses";
